76d08e6a40dae8d8daeb3eab6585289fe14e4e22
Atharva <120232045+AtharvaBhogle@users.noreply.github.com>
2024-07-09 15:39:28 +0000

    message

--- JS_BASICS02/objects.js
+++ JS_BASICS02/objects.js
@@ -3,12 +3,33 @@
 //Object.create
 
 //Object Literals
+const sym=Symbol("atharva")
 const newObj={
     name:"ATHRVA",
     age:20,
+    [sym]:"MySymbol",
     location:"VIRAR",
     lastLoggedIn:["Friday","Saturday"],
     isLoggedIn:true
 }
 //console.log(newObj.name);
-console.log(newObj["name"]);
+//console.log(newObj["name"]);  
+//console.log(newObj[sym]);// it prints the correct value but when we check its datatype it is string which need to be symbol so to
+//do that we write the sym inn square brackets like[sym] and to print it you do not need to add "" inside the brackets like other keys in the bjct
+// to do changes in the key in the object
+newObj.name="ATHARVABHOGLE"
+//console.log(newObj["name"]);// this prints the changed value from the object
+// Object.freeze(newObj)//this will nullify all the changes made to the object after this statement is written
+newObj.name="ATHARVABHOGLE@gdhicbsdhc"
+//console.log(newObj);//no changes from the above statemnet have taken place
+newObj.greeting=function(){
+    console.log("Welcome user");
+}
+console.log(newObj.greeting);// gives reference of the func but doesnt executes it
+console.log(newObj.greeting());// executes the function and gives the value
+// to use keys from the same object
+newObj.newFun=function(){
+    console.log(`welcome user,${this.name}`);// by using the this keyword you can use the key of the same obj
+}
+console.log(newObj.newFun());
+
